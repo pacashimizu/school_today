40f9b2d0eaf0c02aad91762cac8ac5dc2b02a69d
pac.a.shimizu <pac.a.shimizu@gmail.com>
2022-06-22 04:27:36 +0000

    modified for aws s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,16 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory =  ENV['AWS_S3_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
       region: ENV['AWS_S3_REGION'],
       aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      path_style: true
     }
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+
   end
 end
